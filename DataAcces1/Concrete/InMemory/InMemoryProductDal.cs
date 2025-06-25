c5e769bc2c37d7e0987253e59cd4e18e3e1e1035
benpıroyum <birincialikemal18@gmail.com>
2025-06-25 11:53:55 +0300

    Add project files.

---
+++ DataAcces1/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,34 @@
+﻿using DataAcces1.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces1.Concrete.InMemory
+{
+    List<Product> _products;
+    public class InMemoryProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
